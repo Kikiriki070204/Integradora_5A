ab3d8060c09201e165295cef620dcd358e3f594b
imac <kassandritooo@gmail.com>
2024-03-25 14:43:24 -0600

    Initial Commit

---
+++ Integradora_5A/LogInViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LogInViewController.swift
+//  Integradora_5A
+//
+//  Created by imac on 25/03/24.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    @IBOutlet weak var btnLogIn: UIButton!
+    @IBOutlet weak var txtCorreo: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
---
+++ Integradora_5A/RegistroViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RegistroViewController.swift
+//  Integradora_5A
+//
+//  Created by imac on 25/03/24.
+//
+
+import UIKit
+
+class RegistroViewController: UIViewController {
+
+    @IBOutlet weak var txtNombre: UITextField!
+    @IBOutlet weak var txtApellidos: UITextField!
+    @IBOutlet weak var txtCorreo: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+     
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
