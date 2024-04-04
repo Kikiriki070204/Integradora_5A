ec88836296cf55c62bf9589ef43bf4db55a8849b
imac <kassandritooo@gmail.com>
2024-04-04 13:29:57 -0600

    Kiara y Jatziri 2

---
+++ Integradora_5A/VerificarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VerificarViewController.swift
+//  Integradora_5A
+//
+//  Created by imac on 04/04/24.
+//
+
+import UIKit
+
+class VerificarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
